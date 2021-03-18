206fd254393697cb8c82326a492664b79ea13f90
lukasz protosawicki <lukas.es.p@o2.pl>
2021-03-18 22:46:15 +0100

    letters button

--- explorer-hangman/app/Game.js
+++ explorer-hangman/app/Game.js
@@ -1,5 +1,16 @@
 class Game {
+  constructor({ lettersWrapper, categoryWrapper, wordWrapper, outpuWrapper }) {
+    this.lettersWrapper = lettersWrapper;
+    this.categoryWrapper = categoryWrapper;
+    this.wordWrapper = wordWrapper;
+    this.outpuWrapper = outpuWrapper;
+  }
   start() {
-    console.log("lets play the game!");
+    for (let i = 0; i < 26; i++) {
+      const label = (i + 10).toString(36);
+      const button = document.createElement("button");
+      button.innerHTML = label;
+      this.lettersWrapper.appendChild(button);
+    }
   }
 }
