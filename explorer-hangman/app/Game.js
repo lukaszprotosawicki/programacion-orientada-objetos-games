e33c3401289058c08e2e83563170d077130846c4
lukasz protosawicki <lukas.es.p@o2.pl>
2021-03-18 22:51:39 +0100

    WIP

--- explorer-hangman/app/Game.js
+++ explorer-hangman/app/Game.js
@@ -5,11 +5,16 @@ class Game {
     this.wordWrapper = wordWrapper;
     this.outpuWrapper = outpuWrapper;
   }
+
+  guess(letter) {
+    console.log(letter);
+  }
   start() {
     for (let i = 0; i < 26; i++) {
       const label = (i + 10).toString(36);
       const button = document.createElement("button");
       button.innerHTML = label;
+      button.addEventListener("click", () => this.guess(label));
       this.lettersWrapper.appendChild(button);
     }
   }
