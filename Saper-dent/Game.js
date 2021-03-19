fd2bc94e8e7e388bfcdcb45eb1b240242302d6df
lukasz protosawicki <lukas.es.p@o2.pl>
2021-03-19 16:43:32 +0100

    add cell.js + constructor

--- Saper-dent/Game.js
+++ Saper-dent/Game.js
@@ -1,3 +1,5 @@
+import { Cell } from "./Cell.js";
+
 class Game {
   #config = {
     easy: {
@@ -16,7 +18,34 @@ class Game {
       mines: 99,
     },
   };
-  initializeGame() {}
+
+  #numberOfRows = null;
+  #numberOfCols = null;
+  #numberOfMines = null;
+
+  #cells = [];
+  initializeGame() {
+    this.#newGame();
+  }
+  #newGame(
+    rows = this.#config.easy.rows,
+    cols = this.#config.easy.cols,
+    mines = this.#config.easy.mines
+  ) {
+    this.#numberOfRows = rows;
+    this.#numberOfCols = cols;
+    this.#numberOfMines = mines;
+
+    this.#generateCells();
+  }
+  #generateCells() {
+    for (let row = 0; row < this.#numberOfRows; row++) {
+      this.#cells[row] = [];
+      for (let col = 0; col < this.#this.#numberOfCols; col++) {
+        this.#cells[row].push(new Cell(col, row));
+      }
+    }
+  }
 }
 
 window.onload = function () {
