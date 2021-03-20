2c14c62a5f2181d96815f0f8ea758add3e981672
lukasz protosawicki <lukas.es.p@o2.pl>
2021-03-20 14:31:13 +0100

    counter.js add value increment and decrement

--- Saper-dent/Counter.js
+++ Saper-dent/Counter.js
@@ -1,3 +1,29 @@
 import { UI } from "./UI";
 
-export class Counter extends UI {}
+export class Counter extends UI {
+  value = null;
+  #element = null;
+
+  init() {
+    this.#element = this.getElement(this.UiSelectors.counter);
+  }
+
+  setValue(value) {
+    this.value = value;
+    this.#updateValue();
+  }
+
+  increment() {
+    this.value++;
+    this.#updateValue();
+  }
+
+  decrement() {
+    this.value--;
+    this.#updateValue();
+  }
+
+  #updateValue() {
+    this.#element.textContent = this.value;
+  }
+}
